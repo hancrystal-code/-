a0c5baa76f7f0a316731fba7e61e258edc98bf62
hancrystal <hancrystal.com@gmail.com>
2021-05-24 14:04:47 +0900

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     #장고에게 views.post_list를 보여주라고 말해주는 패턴이다.
     #전체 URL 경로에서 접두어(prefix)에 포함되는 도메인 이름(i.e. http://127.0.0.1:8000/)을 무시하고 받아들임
     #마지막 부분인 name='post_list'는 URL에 이름을 붙인 것으로 뷰를 식별
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    # post/기본키/ 경로로 접근 시, post_detail 뷰 보여주기
+    
 ]
